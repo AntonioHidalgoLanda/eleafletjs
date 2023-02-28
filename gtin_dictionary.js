8b6471fb0e8b08bcb6bbdf52fc5283b1256a292f
Antonio Hidalgo Landa MSc MBCS CITP <hidalgo.landa@gmail.com>
2023-02-28 16:49:01 +0000

    Adding examples from Gert
    Adding examples from Gert:
    05909990035946
    04150047731828
    04054839365331
    04054839462153

--- gtin_dictionary.js
+++ gtin_dictionary.js
@@ -45,6 +45,10 @@ var pages_dictionary = {
     '5018431':{"gcp":"5018431","page":"https://serono.merckgroup.com/products?gtin=<gtin>"},
     '4065265':{"gcp":"4065265","page":"https://merckgroup.com/products?gtin=<gtin>"},
     '8806617':{"gcp":"8806617","page":"https://korea.merckgroup.com/products?gtin=<gtin>"},
+    '590999003594':{"gcp":"590999003594","page":"https://www.merckgroup.com/content/dam/web/corporate/non-images/product-information/pl/erbitux/UKR-PIL-Erbitux-5-VAR-IA-ATC-code-May-2022.pdf?gtin=<gtin>"},
+    '415004773182':{"gcp":"415004773182","page":"https://www.gebrauchsinformation4-0.de/gi/rebif-8-8-22-mikrogramm-injektionslosung-in-einer-fertigspritze~018136/?gtin=<gtin>"},
+    '405483936533':{"gcp":"405483936533","page":"https://www.merckgroup.com/content/dam/web/corporate/non-images/product-information/pl/mavenclad/UKR-PIL-Mavenclad-10-VAR-R-Renewal-April-2022.pdf?gtin=<gtin>"},
+    '405483946215':{"gcp":"405483946215","page":"https://www.merckgroup.com/br-pt/bulario/Bavencio_BulaPaciente_010722.pdf?gtin=<gtin>"},
     '5011986':{"gcp":"5011986","page":"https://vogels.co.uk/products?gtin=<gtin>"},
     '95212':{"gcp":"95212","page":"https://www.tracelink.com/?gtin=<gtin>"}
 }
